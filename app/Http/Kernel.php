e7c34473838508df6b157a5d78a1277ecc98d6e2
root <root@dev-crm-dm.lxd>
2019-11-05 08:32:06 +0000

    fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Cors::class,
-        \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
@@ -43,6 +41,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \Illuminate\Session\Middleware\StartSession::class,
+	    \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -63,7 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \App\Http\Middleware\Cors::class,
         
     ];
 
